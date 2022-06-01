297a8b4a8b4316c41260e89bb1ca666a4dcbdc7d
aqvi1a <93330418+aqvi1a@users.noreply.github.com>
2022-06-01 20:52:18 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+import os
 
 
 app = Flask(__name__)
@@ -14,4 +15,5 @@ def face():
 
 
 if __name__ == '__main__':
-    main()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
